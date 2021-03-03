535e2fe55c26591bad251a7daca6be9609c73dbe
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-03-03 17:54:28 -0300

    copia

--- clase01/main.c
+++ clase01/main.c
@@ -11,5 +11,15 @@ int main(int argc, char **argv) {
   cta1.saldo = 4200;
   cta1.cuenta = 1010101;
   cta1.RUT = "8.222.111-1";
+
+  printf("%s\n", cta1.nombre);
+  printf("%f\n", cta1.saldo);
+  printf("%li\n", cta1.cuenta);
+  printf("%s\n", cta1.RUT);
+
+  printf("%s\n", cta2.nombre);
+  printf("%f\n", cta2.saldo);
+  printf("%li\n", cta2.cuenta);
+  printf("%s\n", cta2.RUT);
   return 0;
 }
