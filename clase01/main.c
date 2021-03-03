bdb33d07eeef37bec3150aa3a26d3b7f20c260ef
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-03-03 17:48:53 -0300

    struct

--- clase01/main.c
+++ clase01/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 struct cuenta_banco {
-  char nombre[80];
+  char *nombre;
   float saldo;
   long int cuenta;
-  char RUT[15];
+  char *RUT;
 };
 int main(int argc, char **argv) {
   struct cuenta_banco cta1, cta2, cta3;
+  cta1.nombre = "Bill Gates";
+  cta1.saldo = 4200;
+  cta1.cuenta = 1010101;
+  cta1.RUT = "8.222.111-1";
   return 0;
 }
