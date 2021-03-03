16c55392a5572b198bc63b994d1b834b549bcb38
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-03-03 17:18:08 -0300

    con error!

--- clase01/main.c
+++ clase01/main.c
@@ -4,10 +4,16 @@ int main(int argc, char **argv) {
   int x = 5;
   int y;
   char c[] = "Xasd";
+  char nombre[20];
+  char aux[];
+
   printf("%i %i\n", x, x);
   printf("%s\n", c);
   printf("Hola!\n");
   scanf("%i", &y);
   printf("El valor ingresado fue %i", y);
-  return 0;
+  scanf("%s", nombre);
+  printf("El valor ingresado fue %s", nombre);
+
+    return 0;
 }
