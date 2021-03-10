e99f15e42214ce1bc6385296ce0a3bb1b3624df0
Danilo Bórquez Paredes <danilo.borquez.p@uai.cl>
2021-03-10 16:11:29 -0300

    punteors

--- clase02/ejemplo1.c
+++ clase02/ejemplo1.c
@@ -3,10 +3,15 @@
 int main(int argc, char **argv)
 {
     int x = 123;
-    int *puntero = &x;
+    int *puntero; //int *puntero = &x;
+
+    puntero = &x;
 
     printf("Valor: %i \nDirección: %p\n", x, &x);
     printf("\nValor puntero: %p \nDirección puntero: %p\nContenido apuntado: %i\n", puntero, &puntero, *puntero);
+
+    *puntero = 25;
+    printf("\nx: %i \npuntero: %i\n", x, *puntero);
 
     return 0;
 }
