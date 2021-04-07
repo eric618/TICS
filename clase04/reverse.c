611eb5e786fdd2f3eafffe5e9644a404accdc20a
Danilo Bórquez-Paredes <danilo.borquez.p@uai.cl>
2021-04-07 16:23:45 -0400

    Bubble sort

--- clase04/reverse.c
+++ clase04/reverse.c
@@ -1,10 +1,23 @@
+#include <math.h>
 #include <stdio.h>
 
-// int r(int x) {}
+void f2(int n) {
+  if (n == 0) {
+    return;
+  }
+  printf("%d", n % 10);
+  f2(n / 10);
+}
+
+int f(int x) {
+  int resto = x % 10;
+  int resultado = resto * pow(10, (int)log((double)x));
+}
 
 int main(int argc, char* argv[]) {
   // printf("%i\n", f(6543));
-  int x = 3456;
+  int x = 6813543;
+  int y = x;
   int resto;
   int resultado;
   int suma = 0;
@@ -15,5 +28,7 @@ int main(int argc, char* argv[]) {
     suma += resto;
   }
   printf("%i\n", suma);
+  f2(y);
+  printf("\n");
   return 0;
 }
