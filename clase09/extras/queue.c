d60db772c6d70d424b1bf20d1f2b7a6c12499cf3
Sebastián Sáez <otrab86@gmail.com>
2021-05-07 16:58:31 -0400

    add Makefile

--- clase09/extras/queue.c
+++ clase09/extras/queue.c
@@ -1,40 +1,28 @@
 // codigo fuente que implementa colas con listas enlazadas
-#include <stdio.h> 
-#include <stdlib.h> 
-
-// estructura para representar un nodo
-struct QNode { 
-	int data; 
-	struct QNode* next; 
-}; 
-
-// La lista, primero almacena nodo más antiguo. ultimo almacena mas nuevo
-struct Queue { 
-	struct QNode *primero, *ultimo; 
-}; 
+#include "queue.h"
 
 // funcion para crear un nuevo nodo
-struct QNode* newNode(int k) 
+QNode* newNode(int k) 
 { 
-	struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode)); 
+	QNode* temp = ( QNode*)malloc(sizeof( QNode)); 
 	temp->data = k; 
 	temp->next = NULL; 
 	return temp; 
 } 
 
 // funcion para crear una cola
-struct Queue* createQueue() 
+Queue* createQueue() 
 { 
-	struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue)); 
+	 Queue* q = ( Queue*)malloc(sizeof( Queue)); 
 	q->primero = q->ultimo = NULL; 
 	return q; 
 } 
 
 // Agrega un elemento en la cola (encolar)
-void enQueue(struct Queue* q, int k) 
+void enQueue( Queue* q, int k) 
 { 
 	// crea un nuevo nodo
-	struct QNode* temp = newNode(k); 
+	 QNode* temp = newNode(k); 
 
 	// en caso de que la cola este vacía. El nuevo nodo es ultimo y primero
 	if (q->ultimo == NULL) { 
@@ -48,14 +36,14 @@ void enQueue(struct Queue* q, int k)
 } 
 
 // Saca un elemento de la cola (des encolar)
-void deQueue(struct Queue* q) 
+void deQueue( Queue* q) 
 { 
 	// si la cola está vacía, retorna NULL
 	if (q->primero == NULL) 
 		return; 
 
     // guarda el que estaba primero y se actualiza el primero
-	struct QNode* temp = q->primero; 
+	 QNode* temp = q->primero; 
 	q->primero = q->primero->next; 
 
     // si primero es nulo (el unico en la cola), el ultimo se deja nulo 
@@ -64,21 +52,3 @@ void deQueue(struct Queue* q)
 
 	free(temp); 
 } 
-
-// simple programa para probar las funciones
-int main() 
-{ 
-	struct Queue* q = createQueue(); 
-	enQueue(q, 10); 
-	enQueue(q, 20); 
-	deQueue(q); 
-	deQueue(q); 
-	enQueue(q, 30); 
-	enQueue(q, 40); 
-	enQueue(q, 50); 
-	deQueue(q); 
-	printf("primero en la lista: %d \n", q->primero->data); 
-	printf("ultimo en la lista: %d \n", q->ultimo->data); 
-	return 0; 
-} 
- 
--- clase09/extras/stack.c
+++ clase09/extras/stack.c
@@ -1,44 +1,39 @@
-// codiugo fuente que implementa stacks con listas enlazadas
+// codigo fuente que implementa stacks con listas enlazadas
 #include <limits.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
-
-// estructura para representar un nodo
-struct StackNode { 
-	int data; 
-	struct StackNode* next; 
-}; 
+#include "stack.h"
 
 // creacion de un nuevo nodo en memoria
-struct StackNode* newNode(int data) 
+ StackNode* newStackNode(int data) 
 { 
-	struct StackNode* stackNode = (struct StackNode*)malloc(sizeof(struct StackNode)); 
+	 StackNode* stackNode = ( StackNode*)malloc(sizeof( StackNode)); 
 	stackNode->data = data;
 	stackNode->next = NULL; 
 	return stackNode; 
 } 
 
 // revisa si está vacío
-int isEmpty(struct StackNode* top) 
+int isEmpty(StackNode* top) 
 { 
 	return !top; 
 } 
 
 // agregar un elemento
-void push(struct StackNode** top, int data) 
+void push(StackNode** top, int data) 
 { 
-	struct StackNode* temp = newNode(data);
+	 StackNode* temp = newStackNode(data);
 	temp->next = *top; 
 	*top = temp; 
 	printf("%d agregado a la pila\n", data); 
 } 
 
 // sacar un elemento
-int pop(struct StackNode** top) 
+int pop(StackNode** top) 
 { 
 	if (isEmpty(*top)) 
 		return INT_MIN; // viene de #include <limits.h>
-	struct StackNode* temp = *top; 
+	 StackNode* temp = *top; 
 	*top = (*top)->next; 
 
 	int popped = temp->data; // imprimir
@@ -48,24 +43,9 @@ int pop(struct StackNode** top)
 } 
 
 // mirar sin sacar
-int peek(struct StackNode* top) 
+int peek( StackNode* top) 
 { 
 	if (isEmpty(top)) 
 		return INT_MIN; // viene de #include <limits.h>
 	return top->data; 
-} 
-
-int main() 
-{ 
-	struct StackNode* top = NULL; 
-
-	push(&top, 10); 
-	push(&top, 20); 
-	push(&top, 30); 
-
-	printf("%d sacado de la pila\n", pop(&top)); 
-
-	printf("último elemento agregado:  %d\n", peek(top)); 
-
-	return 0; 
 } 
